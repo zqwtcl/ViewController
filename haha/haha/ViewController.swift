473f2941db0ceb6f70ed65c678ae66a5d4d367d5
zqw <zqw@zqwdeMacBook-Pro-3.local>
2016-04-06 21:45:05 +0800

    怎么回事

---
+++ haha/haha/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  haha
+//
+//  Created by zqw on 16/4/6.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let btn = UIButton()
+        btn.backgroundColor = UIColor.darkGrayColor()
+        
+        self.view.addSubview(btn)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
